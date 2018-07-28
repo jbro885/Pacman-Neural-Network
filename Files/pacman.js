3c73f37635e1b6ec24d6a2b4ecf66666a2d54fcc
Leroy <4c65726f79@gmail.com>
2018-07-28 05:28:20 +0200

    Testing Grid. Just for the Brain

--- Files/pacman.js
+++ Files/pacman.js
@@ -14,8 +14,8 @@ function mutate(x) {
 class PacMan {
   constructor(brain) {
     // position and size of start Pacman
-    this.x = 13; //13.5
-    this.y = 23;  //23
+    this.x = 10; //13.5
+    this.y = 10;  //23
     this.r = 10;
 
     //-1 wegen 0
@@ -38,7 +38,7 @@ class PacMan {
     //   this.brain = brain.copy();
     //   this.brain.mutate(mutate);
     // } else {
-      this.brain = new NeuralNetwork(9, 64, 1);
+      this.brain = new NeuralNetwork(7, 32, 4);
     // }
 
     // Score is how many frames it's been alive
@@ -113,96 +113,102 @@ class PacMan {
       if (FieldData[iv4-1][iv3] == 1) {
         inputs[0] = 0;
       } else if (FieldData[iv4-1][iv3] == 3) {
-        inputs[0] = 0.5;
+        inputs[0] = 1
       } else {
         inputs[0] = 1;
       }
 
       // uper2 field
-      if (iv4 >= 2) {
-        if (FieldData[iv4-2][iv3] == 1) {
-          inputs[1] = 0;
-        } else if (FieldData[iv4-2][iv3] == 3) {
-          inputs[1] = 0.5;
-        } else {
-          inputs[1] = 1;
-        }
-      } else {
-        inputs[1] = 0;
-      }
+      // if (iv4 >= 2) {
+      //   if (FieldData[iv4-2][iv3] == 1) {
+      //     inputs[1] = 0;
+      //   } else if (FieldData[iv4-2][iv3] == 3) {
+      //     inputs[1] = 0.5;
+      //   } else {
+      //     inputs[1] = 1;
+      //   }
+      // } else {
+      //   inputs[1] = 0;
+      // }
 
       // lower field
       if (FieldData[iv4+1][iv3] == 1) {
-        inputs[2] = 0;
+        inputs[1] = 0;
       } else if (FieldData[iv4+1][iv3] == 3) {
-        inputs[2] = 0.5;
+        inputs[1] = 1
       } else {
-        inputs[2] = 1;
+        inputs[1] = 1;
       }
 
-      // lower2 field
-      if (iv4 > FieldData.length-1) {
-        if (FieldData[iv4+2][iv3] == 1) {
-          inputs[3] = 0;
-        } else if (FieldData[iv4+2][iv3] == 3) {
-          inputs[3] = 0.5;
-        } else {
-          inputs[3] = 1;
-        }
-      } else {
-        inputs[3] = 0;
-      }
+      // // lower2 field
+      // if (iv4 > FieldData.length-1) {
+      //   if (FieldData[iv4+2][iv3] == 1) {
+      //     inputs[3] = 0;
+      //   } else if (FieldData[iv4+2][iv3] == 3) {
+      //     inputs[3] = 0.5;
+      //   } else {
+      //     inputs[3] = 1;
+      //   }
+      // } else {
+      //   inputs[3] = 0;
+      // }
 
 
       // Left field
       if (FieldData[iv4][iv3-1] == 1) {
-        inputs[4] = 0;
+        inputs[2] = 0;
       } else if (FieldData[iv4][iv3-1] == 3) {
-        inputs[4] = 0.5;
+        inputs[2] = 1
+      } else {
+        inputs[2] = 1;
+      }
+
+      // if (iv3 >= 2) {
+      //   // Left2 field
+      //   if (FieldData[iv4][iv3-2] == 1) {
+      //     inputs[5] = 0;
+      //   } else if (FieldData[iv4][iv3-2] == 3) {
+      //     inputs[5] = 0.5;
+      //   } else {
+      //     inputs[5] = 1;
+      //   }
+      // } else {
+      //   inputs[5] = 0;
+      // }
+      // Right field
+      if (FieldData[iv4][iv3+1] == 1) {
+        inputs[3] = 0;
+      } else if (FieldData[iv4][iv3+1] == 3) {
+        inputs[3] = 1
+      } else {
+        inputs[3] = 1;
+      }
+
+      // if (iv3 > FieldData[0].length-1) {
+      //   // Right2 field
+      //   if (FieldData[iv4][iv3+2] == 1) {
+      //     inputs[7] = 0;
+      //   } else if (FieldData[iv4][iv3+2] == 3) {
+      //     inputs[7] = 0.5;
+      //   } else {
+      //     inputs[7] = 1;
+      //   }
+      // } else {
+      //   inputs[7] = 0;
+      // }
+
+      // Current
+      if (FieldData[iv4][iv3] == 4) {
+        inputs[4] = 0;
       } else {
         inputs[4] = 1;
       }
 
-      if (iv3 >= 2) {
-        // Left2 field
-        if (FieldData[iv4][iv3-2] == 1) {
-          inputs[5] = 0;
-        } else if (FieldData[iv4][iv3-2] == 3) {
-          inputs[5] = 0.5;
-        } else {
-          inputs[5] = 1;
-        }
-      } else {
-        inputs[5] = 0;
-      }
-      // Right field
-      if (FieldData[iv4][iv3+1] == 1) {
-        inputs[6] = 0;
-      } else if (FieldData[iv4][iv3+1] == 3) {
-        inputs[6] = 0.5;
-      } else {
-        inputs[6] = 1;
-      }
 
-      if (iv3 > FieldData[0].length-1) {
-        // Right2 field
-        if (FieldData[iv4][iv3+2] == 1) {
-          inputs[7] = 0;
-        } else if (FieldData[iv4][iv3+2] == 3) {
-          inputs[7] = 0.5;
-        } else {
-          inputs[7] = 1;
-        }
-      } else {
-        inputs[7] = 0;
-      }
-
-      // Current
-      if (FieldData[iv4][iv3] == 4) {
-        inputs[8] = 0;
-      } else {
-        inputs[8] = 1;
-      }
+      // inputs[5] = 1;
+      inputs[5] = map(iv3, 0, 29, 0, 1);
+      // inputs[6] = 1;
+      inputs[6] = map(iv4, 0, 33, 0, 1);
 
       // console.log("up: "+inputs[0]);
       // console.log("low: "+inputs[1]);
@@ -265,23 +271,33 @@ class PacMan {
   //   }
   // }
   directonstate(action) {
-      if (action[0]<=0.25) {
-        return 0;
-      } else if (action[0]<=0.5) {
-        return 1;
-      } else if (action[0]<=0.75) {
-        return 2;
-      } else if (action[0]<=1) {
-        return 3;
-      }
-      // let mx = Math.max(action[0], action[1], action[2], action[3]);
-      // if (action[0] === mx) {
+      // if (action[0]<=0.25) {
+      //   return 0;
+      // } else if (action[0]<=0.5) {
+      //   return 1;
+      // } else if (action[0]<=0.75) {
+      //   return 2;
+      // } else if (action[0]<=1) {
+      //   return 3;
+      // }
+      let mx = Math.max(action[0], action[1], action[2], action[3]);
+      if (action[0] === mx) {
+          return 0;
+        } else if (action[1] === mx) {
+            return 1;
+          } else if (action[2] === mx) {
+              return 2;
+            } else if (action[3] === mx) {
+                return 3;
+              }
+      // let mx = Math.max(action[0], action[1]);
+      // if (action[0] === mx && action[0] <= 0.5) {
       //     return 0;
-      //   } else if (action[1] === mx) {
+      //   } else if (action[0] === mx && action[0] > 0.5) {
       //       return 1;
-      //     } else if (action[2] === mx) {
+      //     } else if (action[1] === mx && action[1] <= 0.5) {
       //         return 2;
-      //       } else if (action[3] === mx) {
+      //       } else if (action[1] === mx && action[1] > 0.5) {
       //           return 3;
       //         }
 
